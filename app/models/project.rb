a4d5f7ee7413dc27b9b8649b8e485b13941efc3a
trudy macnabb <trudy.macnabb@gmail.com>
2017-01-10 18:24:47 -0500

    changes

--- app/models/project.rb
+++ app/models/project.rb
@@ -23,6 +23,16 @@ BUDGET_RANGE = {
 		['Development', "In this phase the site architecture and visual design will be merged to develop a functional website. All views will be developed to work across modern browser and devices. Interactive elements will be created to make for a more interesting and functional user experience."],
 		['Testing', "In this phase we will work to make sure the site adheres to web standards and functions as expected. This is also the point where you can test out the site and make any necessary revisions." ]
 	]
-	PROJECT_PHASES_RAILS = []
-	PROJECT_PHASES_SHOPIFY = []
+	PROJECT_PHASES_RAILS = [
+		['RAILS - Site Architecture', 'In this phase we will work with you to distill all of the required information for the site. We will discuss what content will likely need to be updated and what can be static. The idea is to create a site that is dynamic with editable content, but not make everything so customizable to the point where maintenance is a headache. We will investigate the best options for video storage and look at designs to see how best to architect the new site. This phase will culminate in the creation of a more detailed spec document that encompasses all features of the build.'],
+		['RAILS - Data Migration', 'In this phase we will go over all components of the current site that need to be migrated to the new site. We will ensure that all data is properly formatted for the new build and complete the necessary migration.' ],
+		['RAILS - Development', "In this phase the site architecture and visual design will be merged to develop a functional website. All views will be developed to work across modern browser and devices. Interactive elements will be created to make for a more interesting and functional user experience."],
+		['RAILS - Testing', "In this phase we will work to make sure the site adheres to web standards and functions as expected. This is also the point where you can test out the site and make any necessary revisions." ]
+	]
+	PROJECT_PHASES_SHOPIFY = [
+		['SHOPIFY - Site Architecture', 'In this phase we will work with you to distill all of the required information for the site. We will discuss what content will likely need to be updated and what can be static. The idea is to create a site that is dynamic with editable content, but not make everything so customizable to the point where maintenance is a headache. We will investigate the best options for video storage and look at designs to see how best to architect the new site. This phase will culminate in the creation of a more detailed spec document that encompasses all features of the build.'],
+		['SHOPIFY - Data Migration', 'In this phase we will go over all components of the current site that need to be migrated to the new site. We will ensure that all data is properly formatted for the new build and complete the necessary migration.' ],
+		['SHOPIFY - Development', "In this phase the site architecture and visual design will be merged to develop a functional website. All views will be developed to work across modern browser and devices. Interactive elements will be created to make for a more interesting and functional user experience."],
+		['SHOPIFY - Testing', "In this phase we will work to make sure the site adheres to web standards and functions as expected. This is also the point where you can test out the site and make any necessary revisions." ]
+	]
 end
